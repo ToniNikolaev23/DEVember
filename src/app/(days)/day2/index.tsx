9b708469fb0559ae6e5d3596865891c627d4df18
Toni Stoyanov <toni@1forfit.com>
2024-01-02 13:16:00 +0200

    Setup expo router

---
+++ src/app/(days)/day2/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const DayDetailsScreen = () => {
+  return (
+    <View>
+      <Text>DayDetailsScreen 2</Text>
+    </View>
+  );
+};
+
+export default DayDetailsScreen;
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { FlatList, StyleSheet, View } from "react-native";
-import DayListItem from "./src/components/core/DayListItem";
+import DayListItem from "../../src/components/core/DayListItem";
 import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
 import {
   AmaticSC_400Regular,
@@ -13,7 +13,7 @@ const days = [...Array(24)].map((val, index) => index + 1);
 
 SplashScreen.preventAutoHideAsync();
 
-export default function App() {
+export default function HomeScreen() {
   const [fontsLoaded, fontError] = useFonts({
     Inter: Inter_900Black,
     Amatic: AmaticSC_400Regular,
